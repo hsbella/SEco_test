3323f699a0ecedb93ebcd031ce71ae9a26b1cb06
hsbella <85680910+hsbella@users.noreply.github.com>
2025-11-30 16:39:49 +0900

    Update Visualization.py

--- pages/Visualization.py
+++ pages/Visualization.py
@@ -7,7 +7,7 @@ st.title("📊 데이터 시각화")
 st.markdown("##### 📈 월별 전체 위험 시계열(Time Series)")
 st.image(Image.open("monthly_timeseries.jpg"), use_column_width=True)
 
-st.markdown("#####🔥 월별 위험 Heatmap")
+st.markdown("##### 🔥월별 위험 Heatmap")
 st.image(Image.open("risk_heatmap.jpg"), use_column_width=True)
 
 st.markdown("##### 🎭 공연별 위험도 Top 10")
