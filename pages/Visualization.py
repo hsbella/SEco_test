4a86acd2132c1305a61a850fba1677a8fcb8ce5f
hsbella <85680910+hsbella@users.noreply.github.com>
2025-11-30 16:39:07 +0900

    Update Visualization.py

--- pages/Visualization.py
+++ pages/Visualization.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-st.title("📊 데이터 시각화 대시보드")
+st.title("📊 데이터 시각화")
 
 
 st.markdown("##### 📈 월별 전체 위험 시계열(Time Series)")
@@ -13,8 +13,8 @@ st.image(Image.open("risk_heatmap.jpg"), use_column_width=True)
 st.markdown("##### 🎭 공연별 위험도 Top 10")
 st.image(Image.open("bar_risk_top10.jpg"), use_column_width=True)
 
-st.markdown("##### 📊 SVI / HLI / AQHI 월별 비교")
+st.markdown("##### 📈 SVI / HLI / AQHI 월별 비교")
 st.image(Image.open("monthly_3index.jpg"), use_column_width=True)
 
-st.markdown("##### 📉 월별 AV-HSI 위험 점수")
+st.markdown("##### 📉 월별 SEco 위험 점수")
 st.image(Image.open("monthly_risk.jpg"), use_column_width=True)
