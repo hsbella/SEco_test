234f5c13df4a9a58054e4c84cdb21c01b622ff56
hsbella <85680910+hsbella@users.noreply.github.com>
2025-11-30 16:46:49 +0900

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,8 +7,10 @@ st.markdown("""
 ### **: 올림픽공원 공연 위험도 예측 시스템**
 """)
 
+---
+
 st.markdown("""
-\n\n\n ### 👋 올공지기 웹사이트에 오신 것을 환영합니다 👋
+\n\n\n\n\n ### 👋 올공지기 웹사이트에 오신 것을 환영합니다 👋
 
 
 공연장(진동·혼잡·공기질) 데이터 통합 기반 위험도 예측 플랫폼입니다.
@@ -22,6 +24,7 @@ st.markdown("""
 - **Predict**<br>Streamlit 기반 UI로 빠른 위험도 예측  
 - **Visualization**<br>월별 시계열·Heatmap·공연별 위험도·지수 비교·Pie chart 시각화 제공
 """, unsafe_allow_html=True)
+
 
 
 
