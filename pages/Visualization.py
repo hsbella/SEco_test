0ebca6429c44bf9a2309eed089a8308c68dfc4f9
hsbella <85680910+hsbella@users.noreply.github.com>
2025-11-30 16:38:15 +0900

    Update Visualization.py

--- pages/Visualization.py
+++ pages/Visualization.py
@@ -4,17 +4,17 @@ from PIL import Image
 st.title("📊 데이터 시각화 대시보드")
 
 
-st.markdown("### 📈 월별 전체 위험 시계열(Time Series)")
+st.markdown("##### 📈 월별 전체 위험 시계열(Time Series)")
 st.image(Image.open("monthly_timeseries.jpg"), use_column_width=True)
 
-st.markdown("###🔥 월별 위험 Heatmap")
+st.markdown("#####🔥 월별 위험 Heatmap")
 st.image(Image.open("risk_heatmap.jpg"), use_column_width=True)
 
-st.markdown("### 🎭 공연별 위험도 Top 10")
+st.markdown("##### 🎭 공연별 위험도 Top 10")
 st.image(Image.open("bar_risk_top10.jpg"), use_column_width=True)
 
-st.markdown("### 📊 SVI / HLI / AQHI 월별 비교")
+st.markdown("##### 📊 SVI / HLI / AQHI 월별 비교")
 st.image(Image.open("monthly_3index.jpg"), use_column_width=True)
 
-st.markdown("### 📉 월별 AV-HSI 위험 점수")
+st.markdown("##### 📉 월별 AV-HSI 위험 점수")
 st.image(Image.open("monthly_risk.jpg"), use_column_width=True)
